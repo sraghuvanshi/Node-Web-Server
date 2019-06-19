0e61a378568b2e0028c2573a41f998c173201f95
rsaksham <sakshamraghuvanshi1@gmail.com>
2019-06-19 21:26:28 +0530

    Projects updated

--- server.js
+++ server.js
@@ -63,6 +63,12 @@ app.get('/bad', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page',
+    });
+});
+
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
